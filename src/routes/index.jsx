47593f10a0534cb247935c13c1bc26786cb4f8aa
Pappas Thomas <thpapei1@gmail.com>
2022-11-27 23:38:59 +0200

    Removed unused imports

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "../pages/dashboard";
-import ErrorPage from "../pages/errorPage";
 import ViewMovieDetails from "../pages/viewMovieDetails";
 
 const router = createBrowserRouter(
