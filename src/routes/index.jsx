4c024ed0e64387e7140b37cce64a8bf78d79c16a
Pappas Thomas <thpapei1@gmail.com>
2022-11-27 23:38:35 +0200

    Fixed basename

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,16 +3,20 @@ import Dashboard from "../pages/dashboard";
 import ErrorPage from "../pages/errorPage";
 import ViewMovieDetails from "../pages/viewMovieDetails";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Dashboard />,
+      // errorElement: <ErrorPage />,
+    },
+    {
+      path: "movie/:imdbId",
+      element: <ViewMovieDetails />,
+    },
+  ],
   {
-    path: "/",
-    element: <Dashboard />,
-    // errorElement: <ErrorPage />,
-  },
-  {
-    path: "movie/:imdbId",
-    element: <ViewMovieDetails />,
-  },
-]);
-
+    basename: "/syntax_exercise_pagination/",
+  }
+);
 export default router;
